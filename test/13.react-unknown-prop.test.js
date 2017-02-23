32dce4ada95216becad2467c193d6b0bad32bd9f
holi <32414154@qq.com>
2017-02-24 05:16:04 +0800

    test React unknown props

---
+++ test/13.react-unknown-prop.test.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {renderIntoDocument as render}from 'react-addons-test-utils';
+
+
+test("unknown property in a dom element will be stripped & warning", (done) => {
+    console.error = (message) => {
+        try {
+            expect(message).toMatch(/Unknown prop `spurious`/);
+            done();
+        } catch (e) {
+            done.fail(e);
+        }
+    };
+
+    let dom = render(<div spurious="true"/>);
+
+    expect(dom.getAttribute('spurious')).toBeUndefined();
+});
+
+
+test("set unknown property in a dom element starts with `data-`  is ok", () => {
+    let dom = render(<div data-spurious="true"/>);
+
+    expect(dom.getAttribute('data-spurious')).toEqual('true');
+});
+
+
+test("unknown property in a Component without an upper case will be stripped but without warning", () => {
+    class foo extends Component {
+        render() {
+            return <span/>
+        }
+    }
+
+    let dom = render(<foo spurious="true"/>);
+
+    expect(dom.getAttribute('spurious')).toBeNull();
+
+});
+
+
