763c9a977504b88abcb20245a8aced8ac134aaef
holi <32414154@qq.com>
2017-02-24 08:27:42 +0800

    test enzyme find component by property

--- test/6.dom.test.js
+++ test/6.dom.test.js
@@ -10,3 +10,14 @@ test('update synchronized', function () {
 
     expect(toy.text()).toBe('bar');
 });
+
+test('find element matching prop starts with `data-*` should be using the origin property name that defined in element', function () {
+    let users = shallow(
+        <div>
+            <div data-userName="jhon">Jhon</div>
+            <div data-userName="mary">Mary</div>
+        </div>
+    );
+
+    expect(users.find({"data-userName": "jhon"}).text()).toEqual("Jhon");
+});
